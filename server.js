101dfacc2b3ff201627c59c392a5d04799deb3f9
Glitch (hello-express) <none>
2020-05-06 10:13:46 +0000

    🎷🐋 Checkpoint ./server.js:15887900/250 ./package.json:15887900/21

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug')
-app.set('view engine', 'pug')
+app.set('views', './views')
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
