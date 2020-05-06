256b318110766ff573f029235152d1af031c023c
Glitch (hello-express) <none>
2020-05-06 23:28:21 +0000

    ⛴🎳 Checkpoint ./.glitch-assets:15887900/465 ./views/todos.pug:15887900/151 ./package.json:15887900/45 ./server.js:15887900/718

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine', 'pug')
 app.set('views', './views')
 
@@ -13,14 +16,20 @@ app.get('/', (request, response) => {
   response.send('I love CodersX');
 });
 
+let todos = [
+  'Đi chợ',
+  'Nấu cơm',
+  'Rửa bát',
+  'Học code tại CodersX'
+]
+
 app.get('/todos', (request, response) => {
-  let todos = [
-    'Đi chợ',
-    'Nấu cơm',
-    'Rửa bát',
-    'Học code tại CodersX'
-  ]
   response.render('todos', { todos: todos});
+});
+
+app.post('/todos/create', (request, response) => {
+  console.log(request.body);
+  todos.push()
 });
 
 // listen for requests :)
